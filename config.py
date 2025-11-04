d7f2dc8b48f1009e1675539d29bb3c256d237e60
Helen <204595204+es-hp@users.noreply.github.com>
2025-11-04 03:13:04 -0500

    Updated ProductionConfig

--- config.py
+++ config.py
@@ -13,5 +13,5 @@ class TestingConfig:
   CACHE_TYPE = 'SimpleCache'
 
 class ProductionConfig:
-  SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+  SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
   CACHE_TYPE = 'SimpleCache'
